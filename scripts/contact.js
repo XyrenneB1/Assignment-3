36f90988c3440d9d82ac67798e1a93743dc9eddf
XyrenneB1 <xyrennethessamae.bastian@edu.sait.ca>
2026-04-11 23:05:00 -0600

    setting the font size to 24 px

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,5 +1,6 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
 
 const thankYouMessage = document.createElement("p");
-
+thankYouMessage.innerHTML = "Thank you for your messagge";
+thankYouMessage.style.fontSize = "24px";
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
