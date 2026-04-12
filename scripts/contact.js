e48bc2a0ac592047fe494dabdd8a23e2873e975c
XyrenneB1 <xyrennethessamae.bastian@edu.sait.ca>
2026-04-11 23:06:34 -0600

    replacing the content of the contact page

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,4 +3,8 @@
 const thankYouMessage = document.createElement("p");
 thankYouMessage.innerHTML = "Thank you for your messagge";
 thankYouMessage.style.fontSize = "24px";
+
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
+
+contactPage.innerHTML = "";
+contactPage.appendChild(thankYouMessage);
