28072908c2a2aa7e2b28be2c874937aabb009cb4
XyrenneB1 <xyrennethessamae.bastian@edu.sait.ca>
2026-04-12 01:16:37 -0600

    contact page edited

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,11 +1,16 @@
 const submitButton = document.getElementById("submit-button");
 const contactPage = document.getElementById("contact-page");
+
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
-const thankYouMessage = document.createElement("p");
-thankYouMessage.innerHTML = "Thank you for your messagge";
-thankYouMessage.style.fontSize = "24px";
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
-// hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
+  const thankYouMessage = document.createElement("p");
+  thankYouMessage.innerHTML = "Thank you for your message!";
+  thankYouMessage.style.fontSize = "24px";
 
-contactPage.innerHTML = "";
-contactPage.appendChild(thankYouMessage);
+  // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
+  thankYouMessage.style.textAlign = "center";
+  contactPage.innerHTML = "";
+  contactPage.appendChild(thankYouMessage);
+});
